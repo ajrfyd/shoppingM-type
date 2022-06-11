c25423c843f8eefbde274f9ac8912f56174c328a
ajrfyd <ajrfyd@naver.com>
2022-06-11 20:49:59 +0900

    Create: New project Initial Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+import { Wrapper } from './App.styles';
 
-function App() {
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  iamge: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
+  
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts, {
+
+  })
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      Hello TypeScript
+    </Wrapper>
   );
 }
 
